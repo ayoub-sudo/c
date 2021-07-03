0768eeda1cbcc2451f2552501851b3f56f8706ee
ayoub-sudo <74210708+ayoub-sudo@users.noreply.github.com>
2021-07-03 16:44:10 +0100

    Add files via upload

---
+++ static/apk.py
@@ -0,0 +1,231 @@
+
+class dataProcess(object):
+    def __init__(self, out_rows, out_cols, data_path="./data/train/images", label_path="./data/train/labels",
+                 test_path="./static/uploads", npy_path="./data/npydata", img_type="jpg"):
+        self.out_rows = out_rows
+        self.out_cols = out_cols
+        self.data_path = data_path
+        self.label_path = label_path
+        self.img_type = img_type
+        self.test_path = test_path
+        self.npy_path = npy_path
+
+   
+
+    def create_test_data(self):
+        i = 0
+        print('Creating test images...')
+        imgs = glob.glob(self.test_path + "/*." + self.img_type)
+        imgdatas = np.ndarray((len(imgs), self.out_rows, self.out_cols, 3), dtype=np.uint8)
+        testpathlist = []
+
+        for imgname in imgs:
+            testpath = imgname
+            testpathlist.append(testpath)
+            img = load_img(testpath, grayscale=False, target_size=[512, 512])
+            img = img_to_array(img)
+            imgdatas[i] = img
+            i += 1
+
+        txtname = './data/results/pic.txt'
+        with open(txtname, 'w') as f:
+            for i in range(len(testpathlist)):
+                f.writelines(testpathlist[i] + '\n')
+        print('loading done')
+        np.save(self.npy_path + '/imgs_test.npy', imgdatas)
+        print('Saving to imgs_test.npy files done.')
+
+    def load_train_data(self):
+        print('load train images...')
+        imgs_train = np.load("./data/npydata" + "/imgs_train.npy")
+        imgs_mask_train = np.load("./data/npydata" + "/imgs_mask_train.npy")
+        imgs_train = imgs_train.astype('float32')
+        imgs_mask_train = imgs_mask_train.astype('float32')
+        imgs_train /= 255
+        imgs_mask_train /= 255
+        imgs_mask_train[imgs_mask_train > 0.5] = 1  
+        imgs_mask_train[imgs_mask_train <= 0.5] = 0 
+        return imgs_train, imgs_mask_train
+
+    def load_test_data(self):
+        print('-' * 30)
+        print('load test images...')
+        print('-' * 30)
+        imgs_test = np.load(self.npy_path + "/imgs_test.npy")
+        imgs_test = imgs_test.astype('float32')
+        imgs_test /= 255
+        return imgs_test
+
+
+
+if __name__ == "__main__":
+    mydata = dataProcess(512, 512)
+
+    mydata.create_test_data()
+
+
+# -*- coding:utf-8 -*-
+
+class myUnet(object):
+      def __init__(self, img_rows=512, img_cols=512):
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+      def load_data(self):
+        mydata = dataProcess(self.img_rows, self.img_cols)
+        imgs_train, imgs_mask_train = mydata.load_train_data()
+        imgs_test = mydata.load_test_data()
+        return imgs_train, imgs_mask_train, imgs_test
+      def get_unet(self):
+        inputs = Input((self.img_rows, self.img_cols, 3))
+
+        conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+        # print(conv1)
+        conv1 = BatchNormalization()(conv1)
+        print ("conv1 shape:", conv1.shape)
+        conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+        conv1 = BatchNormalization()(conv1)
+        print ("conv1 shape:", conv1.shape)
+        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+        print ("pool1 shape:", pool1.shape)
+
+        conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+        print ("conv2 shape:", conv2.shape)
+        conv2 = BatchNormalization()(conv2)
+        conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+        print ("conv2 shape:", conv2.shape)
+        conv2 = BatchNormalization()(conv2)
+        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+        print ("pool2 shape:", pool2.shape)
+
+        conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+        print ("conv3 shape:", conv3.shape)
+        conv3 = BatchNormalization()(conv3)
+        conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+        print ("conv3 shape:", conv3.shape)
+        conv3 = BatchNormalization()(conv3)
+        pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+        print ("pool3 shape:", pool3.shape)
+
+        conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+        conv4 = BatchNormalization()(conv4)
+        conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+        conv4 = BatchNormalization()(conv4)
+        drop4 = Dropout(0.5)(conv4)
+        pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+        conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+        conv5 = BatchNormalization()(conv5)
+        conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+        conv5 = BatchNormalization()(conv5)
+        drop5 = Dropout(0.5)(conv5)
+
+        up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
+        up6 = BatchNormalization()(up6)
+        merge6 = concatenate([drop4, up6], axis=3)
+        print(up6)
+        print(merge6)
+        conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+        print(conv6)
+        conv6 = BatchNormalization()(conv6)
+        conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+        print(conv6)
+        conv6 = BatchNormalization()(conv6)
+        up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            UpSampling2D(size=(2, 2))(conv6))
+        up7 = BatchNormalization()(up7)
+        merge7 = concatenate([conv3, up7], axis=3)
+        print(up7)
+        print(merge7)
+        conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+        conv7 = BatchNormalization()(conv7)
+        print(conv7)
+        conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+        print(conv7)
+        conv7 = BatchNormalization()(conv7)
+        up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            UpSampling2D(size=(2, 2))(conv7))
+        up8 = BatchNormalization()(up8)
+        merge8 = concatenate([conv2, up8], axis=3)
+        conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+        conv8 = BatchNormalization()(conv8)
+        conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+        conv8 = BatchNormalization()(conv8)
+        up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            UpSampling2D(size=(2, 2))(conv8))
+        up9 = BatchNormalization()(up9)
+        merge9 = concatenate([conv1, up9], axis=3)
+        print(up9)
+        print(merge9)
+        conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+        conv9 = BatchNormalization()(conv9)
+        print(conv9)
+        conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+        conv9 = BatchNormalization()(conv9)
+        print(conv9)
+        conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+        conv9 = BatchNormalization()(conv9)
+        print ("conv9 shape:", conv9.shape)
+
+        conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
+        print(conv10)
+        model = Model(inputs=inputs, outputs=conv10)
+
+        model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+
+        return model
+      def train(self):
+        print("loading data")
+        imgs_train, imgs_mask_train, imgs_test = self.load_data()
+        print("loading data done")
+        model = self.get_unet()
+        print("got unet")
+        model_checkpoint = ModelCheckpoint('unet.hdf5', monitor='loss', verbose=1, save_best_only=True)
+        print('Fitting model...')
+        model.fit(imgs_train, imgs_mask_train, batch_size=4, epochs=100, verbose=1,
+                  validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
+        model.save_weights('./data/results/unet_model.hdf5')
+        print('predict test data')
+        imgs_mask_test = model.predict(imgs_test, batch_size=1, verbose=1)
+        np.save('./data/results/imgs_mask_test.npy', imgs_mask_test)
+
+      def save_img(self):
+        print("array to image")
+        imgs = np.load('./data/results/imgs_mask_test.npy')
+        piclist = []
+        for line in open("./data/results/pic.txt"):
+            line = line.strip()
+            picname = line.split('/')[-1]
+            piclist.append(picname)
+        print(len(piclist))
+        for i in range(imgs.shape[0]):
+            path = "./static/" + piclist[i]
+            img = imgs[i]
+            img = array_to_img(img)
+            img.save(path)
+            cv_pic = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+            cv_pic = cv2.resize(cv_pic,(1918,1280),interpolation=cv2.INTER_CUBIC)
+            binary, cv_save = cv2.threshold(cv_pic, 127, 255, cv2.THRESH_BINARY)
+            cv2.imwrite(path, cv_save)
+      def load_model_weights(self, model):
+        model.load_weights('./data/results/unet_model.hdf5')
+
+
+
+
+
+
+myunet = myUnet()
+model = myunet.get_unet()
+    # model.summary()
+    # plot_model(model, to_file='model.png')
+    # Uncomment the below line if you want to re-train a previously trained model 
+myunet.load_model_weights(model)
+imgs_test = mydata.load_test_data()
+imgs_mask_test = model.predict(imgs_test, batch_size=1, verbose=1)
+np.save('./data/results/imgs_mask_test.npy', imgs_mask_test)
+myunet.save_img()
+
+#myunet.train()    
+
+
+
