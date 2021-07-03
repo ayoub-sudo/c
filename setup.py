cb22049170db7ecea58c289552fbc096a762dcbc
ayoub-sudo <74210708+ayoub-sudo@users.noreply.github.com>
2021-07-03 18:06:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
 import numpy as np
 import glob
 import tensorflow
