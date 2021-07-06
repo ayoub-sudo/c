5ae969c45c73f9a2ac5659fb8432fc9b8ad4baaf
ayoub-sudo <74210708+ayoub-sudo@users.noreply.github.com>
2021-07-06 11:28:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -69,5 +69,6 @@ def display_image(filename):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0' , port=8080)
+     port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
  
